bf8e6680a913f5697f203c6ee192f9374fa8adb0
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-01-10 18:18:36 +0800

    feat: debug e9 writer & move syscalls out

---
+++ src/debug_writer.rs
@@ -0,0 +1,47 @@
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use x86_64::instructions::port::Port;
+
+pub struct DebugWriter;
+
+impl DebugWriter {
+    pub fn write_byte(&mut self, byte: u8) {
+        unsafe {
+            Port::new(0xe9).write(byte);
+        }
+    }
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            self.write_byte(byte);
+        }
+    }
+}
+
+impl fmt::Write for DebugWriter {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+lazy_static! {
+    pub static ref WRITER: Mutex<DebugWriter> = Mutex::new(DebugWriter {});
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => (_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => (print!("\n"));
+    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
